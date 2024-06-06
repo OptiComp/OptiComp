24fba7813fb1fc2f03d9ce781d05c55ccebb9834
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 14:01:01 +0200

    Update wrappers_all add hyperopt tpe wrapper

--- opticomp/wrappers/wrappers_all.py
+++ opticomp/wrappers/wrappers_all.py
@@ -2,12 +2,16 @@
 from .optuna.wrapper_optuna_random import OptunaRandomWrapper
 from .optuna.wrapper_optuna_tpe import OptunaTPEWrapper
 
+from .hyperopt.wrapper_hyperopt_tpe import HyperoptTPEWrapper
+
 def get_wrappers():
     # Create wrappers list
     wrappers = []
     # ============================================== Optuna
     wrappers.append(OptunaRandomWrapper)
     wrappers.append(OptunaTPEWrapper)
+    # ============================================== Hyperopt
+    wrappers.append(HyperoptTPEWrapper)
 
     # Return all buildin wrappers
     return wrappers
