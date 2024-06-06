0d586f28c86c7efaaf37c767ab448543df2bc5cc
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 11:30:07 +0200

    Disable optuna wrapper logging

--- opticomp/wrappers/optuna/wrapper_optuna_tpe.py
+++ opticomp/wrappers/optuna/wrapper_optuna_tpe.py
@@ -1,5 +1,6 @@
 # Imports
 import optuna
+import logging
 from ...wrapper_interface import WrapperInterface
 
 
@@ -7,8 +8,12 @@ class OptunaTPEWrapper(WrapperInterface):
     name = "OptunaTPE"  # Name for the wrapper
 
     def optimize(self):
+        # Disable feedback from Optuna
+        optuna.logging.disable_default_handler()
+        optuna.logging.get_logger("optuna").addHandler(logging.NullHandler())
+
         def optuna_objective(trial):
-            params = [trial.suggest_uniform(name, low, high) for name, (low, high) in self.search_space.items()]
+            params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
             return self.objective(params)
         
         study = optuna.create_study(direction='minimize', sampler=optuna.samplers.RandomSampler())
