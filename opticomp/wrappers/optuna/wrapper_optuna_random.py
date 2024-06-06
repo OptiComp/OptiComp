7231c73136739ec89e5f3649dd6c468fb2e3b24e
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 11:49:23 +0200

    Update wrappers to set direction

--- opticomp/wrappers/optuna/wrapper_optuna_random.py
+++ opticomp/wrappers/optuna/wrapper_optuna_random.py
@@ -6,16 +6,21 @@ from ...wrapper_interface import WrapperInterface
     
 class OptunaRandomWrapper(WrapperInterface):
     name = "OptunaRandom"  # Name for the wrapper
+    default_direction = "minimize" # Give default direction
 
-    def optimize(self):
+    def optimize(self, invert_direction):
         # Disable feedback from Optuna
         optuna.logging.disable_default_handler()
         optuna.logging.get_logger("optuna").addHandler(logging.NullHandler())
 
+        # Normalize parameters
         def optuna_objective(trial):
             params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
             return self.objective(params)
         
-        study = optuna.create_study(direction='minimize', sampler=optuna.samplers.TPESampler())
+        # Invert direction if required
+        direction = "maximize" if invert_direction else self.default_direction
+        
+        study = optuna.create_study(direction=direction, sampler=optuna.samplers.TPESampler())
         study.optimize(optuna_objective, n_trials=100)
         return study.best_params, study.best_value
--- opticomp/wrappers/optuna/wrapper_optuna_tpe.py
+++ opticomp/wrappers/optuna/wrapper_optuna_tpe.py
@@ -6,17 +6,22 @@ from ...wrapper_interface import WrapperInterface
 
 class OptunaTPEWrapper(WrapperInterface):
     name = "OptunaTPE"  # Name for the wrapper
+    default_direction = "minimize" # Give default direction
 
-    def optimize(self):
+    def optimize(self, invert_direction):
         # Disable feedback from Optuna
         optuna.logging.disable_default_handler()
         optuna.logging.get_logger("optuna").addHandler(logging.NullHandler())
 
+        # Normalize parameters
         def optuna_objective(trial):
             params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
             return self.objective(params)
         
-        study = optuna.create_study(direction='minimize', sampler=optuna.samplers.RandomSampler())
+        # Invert direction if required
+        direction = "maximize" if invert_direction else self.default_direction
+        
+        study = optuna.create_study(direction=direction, sampler=optuna.samplers.RandomSampler())
         study.optimize(optuna_objective, n_trials=100)
         return study.best_params, study.best_value
     
