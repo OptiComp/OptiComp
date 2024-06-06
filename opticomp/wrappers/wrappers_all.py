b5f2a0d59088de2f52159288ba492cf4043bffe8
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 11:33:47 +0200

    Update get_wrappers list

--- opticomp/wrappers/wrappers_all.py
+++ opticomp/wrappers/wrappers_all.py
@@ -3,5 +3,11 @@ from .optuna.wrapper_optuna_random import OptunaRandomWrapper
 from .optuna.wrapper_optuna_tpe import OptunaTPEWrapper
 
 def get_wrappers():
+    # Create wrappers list
+    wrappers = []
+    # ============================================== Optuna
+    wrappers.append(OptunaRandomWrapper)
+    wrappers.append(OptunaTPEWrapper)
+
     # Return all buildin wrappers
-    return [OptunaRandomWrapper, OptunaTPEWrapper]
+    return wrappers
