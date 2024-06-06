00b769ae6f80629e07bcb6ec7b9d069e6ef2e84d
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 12:35:34 +0200

    Update wrappers invert direction is now handled outside custom wrapper

--- opticomp/wrapper_interface.py
+++ opticomp/wrapper_interface.py
@@ -7,5 +7,8 @@ class WrapperInterface:
     def norm_objective(self, params):
         raise NotImplementedError("This method should be overridden to normalize the parameters for the optimizer")
 
+    def reverse_objective(self):
+        self.norm_objective = lambda params: -self.norm_objective(params)
+
     def optimize(self):
         raise NotImplementedError("This method should be overridden by subclasses")
--- opticomp/wrappers/optuna/wrapper_optuna_tpe.py
+++ opticomp/wrappers/optuna/wrapper_optuna_tpe.py
@@ -14,15 +14,13 @@ class OptunaTPEWrapper(WrapperInterface):
         params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
         return self.objective(params)
     
-    def optimize(self, invert_direction):
+    # Apply optimizer
+    def optimize(self):
         # Disable feedback from Optuna
         optuna.logging.disable_default_handler()
         optuna.logging.get_logger("optuna").addHandler(logging.NullHandler())
-
-        # Invert direction if required
-        direction = "maximize" if invert_direction else self.default_direction
         
-        study = optuna.create_study(direction=direction, sampler=optuna.samplers.RandomSampler())
+        study = optuna.create_study(direction="minimize", sampler=optuna.samplers.RandomSampler())
         study.optimize(self.norm_objective, n_trials=100)
         return study.best_params, study.best_value
     
