55491aa8c03a5370632918e7dc6c1b360b3f6a16
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 12:06:28 +0200

    Update separate norm_objective

--- opticomp/wrapper_interface.py
+++ opticomp/wrapper_interface.py
@@ -4,5 +4,8 @@ class WrapperInterface:
         self.objective = objective
         self.search_space = search_space
 
+    def norm_objective(self, params):
+        return self.objective(params)
+
     def optimize(self):
         raise NotImplementedError("This method should be overridden by subclasses")
--- opticomp/wrappers/optuna/wrapper_optuna_tpe.py
+++ opticomp/wrappers/optuna/wrapper_optuna_tpe.py
@@ -9,20 +9,20 @@ class OptunaTPEWrapper(WrapperInterface):
     library_version = "3.6.1"       # The library version that wrapper is based on
     default_direction = "minimize"  # Give default direction
 
+    # Normalize parameters
+    def norm_objective(self, trial):
+        params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
+        return self.objective(params)
+    
     def optimize(self, invert_direction):
         # Disable feedback from Optuna
         optuna.logging.disable_default_handler()
         optuna.logging.get_logger("optuna").addHandler(logging.NullHandler())
 
-        # Normalize parameters
-        def optuna_objective(trial):
-            params = [trial.suggest_float(name, low, high) for name, (low, high) in self.search_space.items()]
-            return self.objective(params)
-        
         # Invert direction if required
         direction = "maximize" if invert_direction else self.default_direction
         
         study = optuna.create_study(direction=direction, sampler=optuna.samplers.RandomSampler())
-        study.optimize(optuna_objective, n_trials=100)
+        study.optimize(self.norm_objective, n_trials=100)
         return study.best_params, study.best_value
     
