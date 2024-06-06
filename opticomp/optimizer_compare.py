90ad3c260322f6cb1193f72aafc7dbff497f4879
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 11:31:19 +0200

    Update OptimizerCompare verbose option

--- opticomp/optimizer_compare.py
+++ opticomp/optimizer_compare.py
@@ -12,17 +12,22 @@ class OptimizerCompare:
         wrapper = wrapper_class(self.objective, self.search_space)
         self.wrappers.append(wrapper)
 
-    def compare(self):
+    def compare(self, verbose=True):
         results = {}
         for wrapper in self.wrappers:
             start_time = time.time()
-            params, loss = wrapper.compare()
+            params, loss = wrapper.optimize()
             elapsed_time = time.time() - start_time
             results[wrapper.__class__.__name__] = {
                 'params': params,
                 'loss': loss,
                 'time': elapsed_time
             }
+
+            if verbose:
+                print(f"Optimiser: {wrapper.__class__.__name__}")
+                print(f"Loss: {loss}")
+                print(f"Time: {elapsed_time}\n")
         
         best_result = min(results.items(), key=lambda x: x[1]['loss'])
         return best_result, results
