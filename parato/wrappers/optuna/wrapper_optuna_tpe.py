0421c1789723a0659009cf2973e3b1e01524a01e
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 10:44:59 +0200

    Create optuna wrappers

---
+++ parato/wrappers/optuna/wrapper_optuna_tpe.py
@@ -0,0 +1,15 @@
+# Imports
+import optuna
+from parato.wrapper_interface import WrapperInterface
+
+class OptunaRandomWrapper(WrapperInterface):
+    name = "OptunaRandom"  # Name for the wrapper
+
+    def optimize(self):
+        def optuna_objective(trial):
+            params = [trial.suggest_uniform(name, low, high) for name, (low, high) in self.search_space.items()]
+            return self.objective(params)
+        
+        study = optuna.create_study(direction='minimize', sampler=optuna.samplers.TPESampler())
+        study.optimize(optuna_objective, n_trials=100)
+        return study.best_params, study.best_value
