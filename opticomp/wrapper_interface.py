ade2ec297f63286f3a25756f4243a64d2991cc8f
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-06 12:09:02 +0200

    Update WrapperInterface normalize

--- opticomp/wrapper_interface.py
+++ opticomp/wrapper_interface.py
@@ -5,7 +5,7 @@ class WrapperInterface:
         self.search_space = search_space
 
     def norm_objective(self, params):
-        return self.objective(params)
+        raise NotImplementedError("This method should be overridden to normalize the parameters for the optimizer")
 
     def optimize(self):
         raise NotImplementedError("This method should be overridden by subclasses")
