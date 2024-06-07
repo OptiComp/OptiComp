8b15b17334344303c5d9fb0c20d9d96b711cb4e6
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-07 15:56:25 +0200

    Test sphere function

--- use_buildin_wrappers.py
+++ use_buildin_wrappers.py
@@ -1,19 +1,35 @@
 # Imports
 from opticomp.benchmarking import OptimizerBenchmark
 from opticomp.wrappers_control import Wrapper
+import numpy as np
 
 # Objective function
+# def objective(params):
+#     param1 = params['param1'] 
+#     param2 = params['param2'] 
+#     # param3 = params['param3'] 
+#     return (param1 - 2) ** 2 + (param2 + 3) ** 2 # - (param3 + 3) ** 2
+
+'''
+Performance on the Sphere function signifies an optimizer's effectiveness in finding the global minimum, 
+situated at the origin with a function value of 0. 
+It reflects the optimizer's ability to efficiently explore the search space, 
+navigate towards the optimal solution, 
+and converge to the global minimum with minimal iterations or evaluations.
+'''
+# sphere function
 def objective(params):
-    param1 = params['param1'] 
-    param2 = params['param2'] 
-    # param3 = params['param3'] 
-    return (param1 - 2) ** 2 + (param2 + 3) ** 2 # - (param3 + 3) ** 2
+    x = np.array([params[key] for key in params.keys()])
+    return np.sum(x**2)
 
 # Search space
 # {'param_name': (min, max)}
-search_space = {'param1': (-10, 10), 
-                'param2': (-10, 10)}
-                # 'param3': (-100, 300),}
+search_space = {'param1': (-100, 100), 
+                'param2': (-100, 100),
+                'param3': (-100, 100),
+                'param4': (-100, 100),
+                'param5': (-100, 100),
+                'param6': (-100, 100)}
 
 # Select wrappers by name
 wrapper_names = ["OptunaRandom", "OptunaTPE", "OptunaGridSearch", "BayesianOpt"]
