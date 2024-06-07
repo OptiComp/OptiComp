fc584a6fda894959d5c205718f04a4c744e4100e
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-07 14:29:25 +0200

    Cleanup WrapperInterface

--- opticomp/wrapper_interface.py
+++ opticomp/wrapper_interface.py
@@ -10,22 +10,22 @@ class WrapperInterface:
     def _wrap_norm_parameters(self, params):
         raise NotImplementedError("This method should be overridden to normalize the parameters for the objective")
 
-    def _wrap_execute_optimization(self):
+    def _wrap_execute_optimization(self, final_objective, n_steps):
         raise NotImplementedError("This method should be overridden by subclasses")
     
     def optimize(self, invert, n_steps):
         # Create the final objective function. Normilize parameters and set the direction. 
         def final_objective(params):
+            # Normalize parameters
             params = self._wrap_norm_parameters(params)
             result = self.objective(params)
-            # Invert result
-            if invert:
-                return -result
-            else:
-                return result
+            # Invert result if needed
+            return -result if invert else result
+
         # Run optimizer
         params, score = self._wrap_execute_optimization(final_objective, n_steps)
-        # Invert final score
-        if invert:
-            score = -score
+        
+        # Invert final score if needed
+        score = -score if invert else score
         return params, score
+    
