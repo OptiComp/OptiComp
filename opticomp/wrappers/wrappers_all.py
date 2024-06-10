bb45225691b7e9cdd334b638556541af0fa6cd95
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-10 10:12:18 +0200

    Cleanup wrappres_all.py

--- opticomp/wrappers/wrappers_all.py
+++ opticomp/wrappers/wrappers_all.py
@@ -1,8 +1,11 @@
 # List of wrapper modules, class names, and optimizer names
 wrapper_info = [
+    # Optuna
     ("opticomp.wrappers.optuna.wrapper_optuna_random", "OptunaRandom"),
     ("opticomp.wrappers.optuna.wrapper_optuna_tpe", "OptunaTPE"),
     ("opticomp.wrappers.optuna.wrapper_optuna_grid_search", "OptunaGridSearch"),
+    # Hyperopt
     ("opticomp.wrappers.hyperopt.wrapper_hyperopt_tpe", "HyperoptTPE"),
+    # Bayesian
     ("opticomp.wrappers.bayes_opt.wrapper_bayesian_optimization", "BayesianOpt"),
 ]
