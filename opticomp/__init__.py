f0076e27b3a66b670c09fd9e2046a1507ef38b7c
SJWRD <117866316+SJWRD@users.noreply.github.com>
2024-06-13 10:08:11 +0200

    Cleanup __init__.py

--- opticomp/__init__.py
+++ opticomp/__init__.py
@@ -3,4 +3,7 @@ from .objective_zoo import objective_zoo as objective_zoo
 from .wrapper_zoo import wrapper_zoo as wrapper_zoo
 from .wrapper_zoo.wrapper_interface import WrapperInterface as WrapperInterface
 
-__all__ = ["WrapperInterface", "objective_zoo", "BenchmarkSuite", "wrapper_zoo"]
+__all__ = ["BenchmarkSuite",
+           "objective_zoo",
+           "wrapper_zoo",
+           "WrapperInterface"]
